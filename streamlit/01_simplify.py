594bca8dffc678cdb28e820858ff836684515e02
Abhiram R <everythingpython0@gmail.com>
2021-10-22 19:24:46 +0530

    Updated code

--- streamlit/01_simplify.py
+++ streamlit/01_simplify.py
@@ -1,17 +1,18 @@
 from os import stat
 import streamlit as st
 import pandas as pd
-pd.get_option("display.max_colwidth",100000)
-status = 0
 
-def display(df):
+pd.get_option("display.max_colwidth",100000)
+
+def display(df,columns_to_display):
     placeholder = st.empty()
     with placeholder.container():
-        st.write(df)
+        st.write(df[columns_to_display])
         st.write(f"Columns in file - {df.columns}")
 
 def filter(df, levels, columns):
     print(levels)
+    df = df.astype(str).apply(lambda x: x.str[:100]).dropna()
     df = df[df['Level']==levels][columns]
     st.table(df)
 
@@ -20,34 +21,38 @@ def load_data(uploaded_file):
     df = pd.read_csv(uploaded_file)
     return df
 
-state = 0
+        
+st.title("Log File Look-up-er", anchor=None)
 
 uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file:
-    df = load_data(uploaded_file)
-    button1 = st.button(label="Display")
 
-    if button1:
-        my_expander = st.container()
-        with my_expander:
-            display(df)
-
-    if df is not None:
-        levels = list(set(df["Level"].to_list()))
-        print(levels)
-        
-
-        option = st.sidebar.selectbox("Filter by:", levels) 
+try:
+    if uploaded_file:
+        df = load_data(uploaded_file)
         columns_to_display = st.multiselect("Columns to display:", df.columns)
+        col1, col2 = st.columns(2)
+        button1 = col1.button(label="Display")
 
-        print(option)
-        if len(option) >0:
-            button2 = st.button(label="Filter")
-            if button2:
-                my_expander = st.container()
-                with my_expander:
-                    filter(df, option, columns_to_display)
+
+        if button1:
+            my_container = st.container() #expander
+            with my_container:
+                display(df, columns_to_display)
+
+        if df is not None:
+            levels = list(set(df["Level"].to_list()))
+            print(levels)
             
+            option = st.sidebar.selectbox("Filter by:", levels) 
+
+            if len(option) >0:
+                button2 = col2.button(label="Filter")
+                if button2:
+                    my_container = st.container()
+                    with my_container:
+                        filter(df, option, columns_to_display)
+except Exception as e:
+    print(f"Error - {e}")
 
 
 
--- streamlit/02_maths_tables.py
+++ streamlit/02_maths_tables.py
@@ -1,5 +1,16 @@
 import streamlit as st
+import plotly.graph_objects as grph_obj
+
+st.title("Math Tables in 2021", anchor=None)
 
 
-age = st.slider('How old are you?', 0, 130, 25)
-st.write("I'm ", age, 'years old')
+num = st.slider('Which times tables do you want to display', 1, 100, 7)
+
+values = [str(num), str(num)+"*x="]
+cell_values = [str(num)+"*"+str(i)+"=" for i in range(1,10)], [num*i for i in range(1,10)]
+
+fig = grph_obj.Figure(data=[go.Table(header=dict(values=values),
+                 cells=dict(values=[cell_values]))
+                     ])
+
+st.write(fig)
