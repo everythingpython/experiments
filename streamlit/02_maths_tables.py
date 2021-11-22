3809c4aea5cf20901988179ae4c74330e8158f78
Abhiram R <everythingpython0@gmail.com>
2021-11-23 00:25:40 +0530

    Added requirements

--- streamlit/02_maths_tables.py
+++ streamlit/02_maths_tables.py
@@ -7,10 +7,8 @@ st.title("Math Tables in 2021", anchor=None)
 num = st.slider('Which times tables do you want to display', 1, 100, 7)
 
 values = [str(num), str(num)+"*x="]
-cell_values = [str(num)+"*"+str(i)+"=" for i in range(1,10)], [num*i for i in range(1,10)]
-
-fig = grph_obj.Figure(data=[go.Table(header=dict(values=values),
-                 cells=dict(values=[cell_values]))
+fig = grph_obj.Figure(data=[grph_obj.Table(header=dict(values=[str(num), str(num)+"*x="]),
+                 cells=dict(values=[[str(num)+"*"+str(i)+"=" for i in range(1,10)], [num*i for i in range(1,10)]]))
                      ])
 
 st.write(fig)
